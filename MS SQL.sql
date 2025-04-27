7b5ba92eb3cce671bfa152f0bf2f799d8e654296
nape10 <karoly.nagy@stud.uni-corvinus.hu>
2025-04-27 18:14:23 +0200

    Add files via upload

--- MS SQL.sql
+++ MS SQL.sql
@@ -1,33 +1,20 @@
-CREATE OR ALTER VIEW VSZOBA AS
-SELECT sz.*,
-szh.SZALLAS_NEV,
-szh.HELY,
-szh.CSILLAGOK_SZAMA
-FROM Szoba sz JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
-SELECT * FROM VSZOBA
-
-CREATE OR ALTER PROCEDURE SPUgyfelfoglalasok
-@ugyfelazon nvarchar(40)
-AS
-BEGIN
-SELECT *
-FROM Foglalas
-WHERE ugyfel_fk = @ugyfelazon
-END
-EXEC SPUgyfelfoglalasok 'laszlo2'
-
-CREATE OR ALTER FUNCTION UDFFerohely
+/*CREATE TABLE Ugyfelek
 (
-  @fazon INT
-)
-RETURNS INT -- az érték típusa
-AS
-BEGIN
-DECLARE @ferohely INT
+login varchar(20) MASKED WITH (Function = 'partial(1,"XXX",1)'),
+email varchar(30) MASKED WITH (function = 'email()'),
+nev varchar(20),
+szulev int MASKED WITH (FUNCTION = 'random(1900,2020)'),
+nem varchar(10),
+cim varchar(50)
+)*/
 
-  SELECT @ferohely = sz.FEROHELY
-  FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
-  WHERE f.FOGLALAS_PK = @fazon
-RETURN @ferohely
-END
-SELECT dbo.UDFFerhely(651)
+INSERT INTO Ugyfelek (login, email, nev, szulev, nem, cim)
+SELECT login, email, nev, szulev, nem, cim
+FROM adatbhzi;
+
+CREATE USER MaskUser WITHOUT Login;
+GRANT SELECT ON Ugyfelek TO MaskUser
+
+EXECUTE AS User= 'MaskUser';
+SELECT * FROM Ugyfelek
+REVERT
